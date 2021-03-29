a4e8a44ff94c4d0c2ed9387f27a486b95ae4d83c
stuyk <stuykgaming@gmail.com>
2021-03-29 10:04:49 -0600

    Fix Dates

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const fetch = require("node-fetch");
+const gitDateExtractor = require('git-date-extractor');
 const fs = require("fs");
 const path = require("path");
 const walk = require("walk");
@@ -18,20 +19,31 @@ walker.on("end", async () => {
   const finalizedFiles = [];
 
   for (let i = 0; i < files.length; i++) {
-    let fileData = JSON.parse(fs.readFileSync(files[i].filePath).toString());
-    fileData.author = files[i].author;
-    const repoPath = fileData.url.replace("https://github.com/", "");
-    const response = await fetch(`https://api.github.com/repos/${repoPath}`);
-    const repoDataString = await response.text();
-    const repoData = JSON.parse(repoDataString);
-    fileData.updated = repoData.updated_at;
-    fileData.creation = repoData.created_at;
-    fileData.pushed = repoData.pushed_at;
-    fileData.stars = repoData.stargazers_count;
-    console.log(fileData.updated);
+    console.log(`Parsing File: ${i}/${files.length}`);
 
+    const fileData = JSON.parse(fs.readFileSync(files[i].filePath).toString());
+    const extraction = await gitDateExtractor.getStamps({ files: [ files[i].filePath ] });
+    const extractionData = extraction[Object.keys(extraction)[0]];
+
+    fileData.author = files[i].author;
+    fileData.updated = new Date(extractionData.modified * 1000).toISOString();
+    fileData.pushed = new Date(extractionData.modified * 1000).toISOString();
+    fileData.creation = new Date(extractionData.created * 1000).toISOString();
+
+    // Rate Limiting will prevent this from properly populating.
+    // Currently we'll use git logs to determine updates.
+    // const repoPath = fileData.url.replace("https://github.com/", "");
+    // const response = await fetch(`https://api.github.com/repos/${repoPath}`);
+    // const repoDataString = await response.text();
+    // const repoData = JSON.parse(repoDataString);
+    // fileData.updated = repoData.updated_at;
+    // fileData.creation = repoData.created_at;
+    // fileData.pushed = repoData.pushed_at;
+    // fileData.stars = repoData.stargazers_count;
+
+    console.log(`Updated: ${fileData.updated}`);
     finalizedFiles.push(fileData);
-    console.log(`Parsed Files: ${i}/${files.length}`);
+    
   }
 
   if (!fs.existsSync(path.join(__dirname, "../", "/dist"))) {
